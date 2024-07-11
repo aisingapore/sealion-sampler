1af1dc5d5b882775d607fbf6bb2d1897a1828a70
Zhi Hao <zhihao.ong.91@gmail.com>
2024-07-11 18:28:22 +0800

    first commit

---
+++ sealion_3b_prompt.py
@@ -0,0 +1,9 @@
+# please use transformers 4.34.1
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("aisingapore/sea-lion-3b", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("aisingapore/sea-lion-3b", trust_remote_code=True)
+
+tokens = tokenizer("The sea lion is a", return_tensors="pt")
+output = model.generate(tokens["input_ids"], max_new_tokens=20, eos_token_id=tokenizer.eos_token_id)
+print(tokenizer.decode(output[0], skip_special_tokens=True))
